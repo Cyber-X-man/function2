614218cf676193b59da2dcaa3edd9c4d8d461725
Cyber-X-man <121112632+Cyber-X-man@users.noreply.github.com>
2023-01-21 19:28:41 +0600

    Add files via upload

---
+++ function2.py
@@ -0,0 +1,31 @@
+def sum():
+    a = 12
+    b = 13
+    c = a + b
+    print(c)
+
+def run(x,y):
+    a = x + y   #print
+    print(a)
+
+def add(x,y):
+    b = x * y  #return
+    return b
+
+def fun(x,y):
+    if x > y:
+        print(x)  #print
+    else:
+        print(y)
+
+def function(x,y):
+    if x < y:
+        return y    #return
+    else:
+        return x
+
+sum()
+run(20, 30)
+print(add(12,24))
+fun(100,90)
+print(function(100,200))
